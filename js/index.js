19fc93a25c9b4c2e0a1706273f5413afc9739a2f
matiz86 <120100418+matiz86@users.noreply.github.com>
2023-12-25 21:10:03 +0300

    main section

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".main__swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".main .arrow__next",
+    prevEl: ".main .arrow__prev",
+  },
+});
