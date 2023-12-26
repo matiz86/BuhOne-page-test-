9416f7f8e7974aa6d2c101716e0963227fe0b6a3
matiz86 <120100418+matiz86@users.noreply.github.com>
2023-12-26 21:51:53 +0300

    clients section

--- js/index.js
+++ js/index.js
@@ -1,10 +1,40 @@
 var swiper = new Swiper(".main__swiper", {
   pagination: {
-    el: ".swiper-pagination",
+    el: ".main .swiper-pagination",
     clickable: true,
   },
   navigation: {
     nextEl: ".main .arrow__next",
     prevEl: ".main .arrow__prev",
+  },
+});
+var swiper = new Swiper(".clients__swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  pagination: {
+    el: ".clients .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".clients .arrow__next",
+    prevEl: ".clients .arrow__prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1010: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
   },
 });
